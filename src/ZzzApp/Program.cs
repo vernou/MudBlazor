57c8b0cfeab1b2e816f4d317d335d86ada59594c
Cedric VERNOU <cedric.vernou@hotmail.com>
2025-02-18 19:15:52 +0100

    wip

---
+++ src/ZzzApp/Program.cs
@@ -0,0 +1,38 @@
+// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using MudBlazor.Services;
+using ZzzApp.Components;
+
+namespace ZzzApp;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add MudBlazor services
+        builder.Services.AddMudServices();
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+        }
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
+    }
+}
